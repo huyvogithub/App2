932dd020a943a9ee4b201e23299f836b8376e3c3
HUYVO <huyvo0188630@gmail.com>
2023-12-30 18:32:21 +0700

    Updates

---
+++ precache-manifest.4b8b9d817d12f4f44e6335258cb0f504.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0d50f435ac7855cc386dcde5cb2630",
+    "url": "/App2/index.html"
+  },
+  {
+    "revision": "850eff8892caeda96995",
+    "url": "/App2/static/css/main.d299af08.chunk.css"
+  },
+  {
+    "revision": "8c5e0424887d65563bdd",
+    "url": "/App2/static/js/2.0d722959.chunk.js"
+  },
+  {
+    "revision": "850eff8892caeda96995",
+    "url": "/App2/static/js/main.74725cb3.chunk.js"
+  },
+  {
+    "revision": "362c7ad51e0d9ed254ab",
+    "url": "/App2/static/js/runtime~main.675c3bd4.js"
+  },
+  {
+    "revision": "91cf9f59983097ceb49ffbcc931fee7b",
+    "url": "/App2/static/media/MOHINH3D.91cf9f59.glb"
+  },
+  {
+    "revision": "dc60471d6ab1a7e4d53ce1d0879ea388",
+    "url": "/App2/static/media/benhnhan.dc60471d.jpg"
+  }
+]);
